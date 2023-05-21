aa8171d1a3961f965d6a0906015ed4b7d61e41fc
Rus Sharafiev <srr@live.ru>
2023-05-21 22:50:45 +0300

    Refactor Use arg to build or dev

--- main.go
+++ main.go
@@ -2,87 +2,24 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"os"
-	"rus-sharafiev/dev-server/fswr"
-	lessplugin "rus-sharafiev/dev-server/less-plugin"
-
-	"github.com/evanw/esbuild/pkg/api"
-	"github.com/gorilla/websocket"
+	"rus-sharafiev/dev-server/build"
+	"rus-sharafiev/dev-server/start"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-var clients []*websocket.Conn
-
-var reloadPlugin = api.Plugin{
-	Name: "reload-plugin",
-	Setup: func(build api.PluginBuild) {
-
-		build.OnEnd(func(result *api.BuildResult) (api.OnEndResult, error) {
-			for _, conn := range clients {
-				conn.WriteMessage(websocket.TextMessage, []byte("reload"))
-			}
-			clients = nil
-			return api.OnEndResult{}, nil
-		})
-
-	},
-}
-
 func main() {
-	ctx, err := api.Context(api.BuildOptions{
-		EntryPoints: []string{"src/index.tsx"},
-		JSXDev:      true,
-		JSX:         api.JSXAutomatic,
-		Bundle:      true,
-		Outdir:      "build",
-		Sourcemap:   api.SourceMapInline,
-		Plugins:     []api.Plugin{reloadPlugin, lessplugin.LessPlugin},
-		External:    []string{"*.gif", "*.eot", "*.woff", "*.ttf"},
-		Banner:      map[string]string{"js": "(() => new WebSocket('ws://localhost:8000/ws').onmessage = () => location.reload())();"},
-		Write:       true,
-		LogLevel:    api.LogLevelInfo,
-	})
 
-	if err != nil {
-		os.Exit(1)
+	if noArgs := len(os.Args); noArgs == 1 {
+		fmt.Println("No argument has been provided")
+		return
 	}
 
-	err2 := ctx.Watch(api.WatchOptions{})
-	if err2 != nil {
-		os.Exit(1)
+	switch script := os.Args[1]; script {
+	case "start":
+		start.Run()
+	case "build":
+		build.Run()
+	default:
+		fmt.Println("Invalid argument. Allowed are 'build' and 'dev'")
 	}
-
-	fmt.Printf("\n\x1b[2mHTTP server is running on http://localhost:8000/\n \x1b[0m ")
-	fmt.Printf("\n\x1b[33m[esbuild] \x1b[0mwatching for changes...\n\n")
-
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		clients = append(clients, conn)
-
-		for {
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
-		}
-	})
-
-	http.Handle("/", http.StripPrefix("/", fswr.FileServerWithRedirect(http.Dir("build/"))))
-
-	log.Fatal(http.ListenAndServe(":8000", nil))
 }
---
+++ start/start.go
@@ -0,0 +1,88 @@
+package start
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"rus-sharafiev/dev-server/fswr"
+	lessplugin "rus-sharafiev/dev-server/less-plugin"
+
+	"github.com/evanw/esbuild/pkg/api"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var clients []*websocket.Conn
+
+var reloadPlugin = api.Plugin{
+	Name: "reload-plugin",
+	Setup: func(build api.PluginBuild) {
+
+		build.OnEnd(func(result *api.BuildResult) (api.OnEndResult, error) {
+			for _, conn := range clients {
+				conn.WriteMessage(websocket.TextMessage, []byte("reload"))
+			}
+			clients = nil
+			return api.OnEndResult{}, nil
+		})
+
+	},
+}
+
+func Run() {
+	ctx, err := api.Context(api.BuildOptions{
+		EntryPoints: []string{"src/index.tsx"},
+		JSXDev:      true,
+		JSX:         api.JSXAutomatic,
+		Bundle:      true,
+		Outdir:      "build",
+		Sourcemap:   api.SourceMapInline,
+		Plugins:     []api.Plugin{reloadPlugin, lessplugin.LessPlugin},
+		External:    []string{"*.gif", "*.eot", "*.woff", "*.ttf"},
+		Banner:      map[string]string{"js": "(() => new WebSocket('ws://localhost:8000/ws').onmessage = () => location.reload())();"},
+		Write:       true,
+		LogLevel:    api.LogLevelInfo,
+	})
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	err2 := ctx.Watch(api.WatchOptions{})
+	if err2 != nil {
+		os.Exit(1)
+	}
+
+	fmt.Printf("\n\x1b[2mHTTP server is running on http://localhost:8000/\n \x1b[0m ")
+	fmt.Printf("\n\x1b[33m[esbuild] \x1b[0mwatching for changes...\n\n")
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		clients = append(clients, conn)
+
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.Handle("/", http.StripPrefix("/", fswr.FileServerWithRedirect(http.Dir("build/"))))
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
