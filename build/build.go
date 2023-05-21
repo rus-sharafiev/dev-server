cd06662f84b95c64ce1494353c21439df0d24dd1
Rus Sharafiev <srr@live.ru>
2023-05-21 23:00:06 +0300

    Refactor minify build

--- build/build.go
+++ build/build.go
@@ -9,16 +9,19 @@ import (
 
 func Run() {
 	result := api.Build(api.BuildOptions{
-		EntryPoints: []string{"src/index.tsx"},
-		JSXDev:      true,
-		JSX:         api.JSXAutomatic,
-		Bundle:      true,
-		Outdir:      "dist",
-		Sourcemap:   api.SourceMapInline,
-		Plugins:     []api.Plugin{lessplugin.LessPlugin},
-		External:    []string{"*.gif", "*.eot", "*.woff", "*.ttf"},
-		Write:       true,
-		LogLevel:    api.LogLevelInfo,
+		EntryPoints:       []string{"src/index.tsx"},
+		JSXDev:            true,
+		JSX:               api.JSXAutomatic,
+		Bundle:            true,
+		MinifyWhitespace:  true,
+		MinifyIdentifiers: true,
+		MinifySyntax:      true,
+		Outdir:            "build",
+		Sourcemap:         api.SourceMapInline,
+		Plugins:           []api.Plugin{lessplugin.LessPlugin},
+		External:          []string{"*.gif", "*.eot", "*.woff", "*.ttf"},
+		Write:             true,
+		LogLevel:          api.LogLevelInfo,
 	})
 
 	if len(result.Errors) > 0 {
