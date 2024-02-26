6e43725aadd4750574168f819a69a3b70320af87
Rus Sharafiev <srr@live.ru>
2024-02-26 15:57:29 +0300

    Add deploying to a path from the conf file

--- commands/deploy/deploy.go
+++ commands/deploy/deploy.go
@@ -1,14 +1,18 @@
 package deploy
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 )
 
-const envFile = "dev.env"
+const confFile = "./dev.conf"
+
+type DevConfig struct {
+	DeployPath string `json:"deployPath"`
+}
 
 func Run(args ...string) {
 	var path string
@@ -16,29 +20,40 @@ func Run(args ...string) {
 	if len(args) >= 1 {
 		path = args[0]
 	} else {
-		if _, err := os.Stat(envFile); os.IsNotExist(err) {
-			log.Fatal("No target path has been provided, e.g. dev deploy root@0.0.0.0:/var/www/html/")
-		} else if err == nil {
-			data, err := os.ReadFile(envFile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			lines := strings.Split(string(data), "\n")
-			for _, line := range lines {
-				part := strings.Split(line, "=")
+		if _, err := os.Stat(confFile); os.IsNotExist(err) {
 
-				if part[0] == "deployPath" {
-					path = part[1]
-				}
+			log.Fatal("No target path has been provided, e.g. dev deploy root@0.0.0.0:/var/www/html/")
+
+		} else if err == nil {
+
+			data, err := os.ReadFile(confFile)
+			if err != nil {
+				log.Fatal("ReadFile err: ", err)
 			}
+
+			var config DevConfig
+			if err = json.Unmarshal(data, &config); err != nil {
+				log.Fatal("Unmarshal err: ", err)
+			}
+
+			path = config.DeployPath
 		}
 	}
 
-	fmt.Println(path)
+	entries, err := os.ReadDir("build")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commandArgs := []string{"-r"}
+	for _, e := range entries {
+		commandArgs = append(commandArgs, "build/"+e.Name())
+	}
+	commandArgs = append(commandArgs, path)
 
 	fmt.Printf("\n%s\n\n", "Copying files via scp...")
 
-	cmd := exec.Command("scp", "-r", "build/*", path)
+	cmd := exec.Command("scp", commandArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
