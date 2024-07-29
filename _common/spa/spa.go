25c1b2a129163d719e1a6a9a043e1ee8b9c83f7d
Rus Sharafiev <srr@live.ru>
2024-07-29 09:56:50 +0300

    Add serving gz files

--- _common/spa/spa.go
+++ _common/spa/spa.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Handler struct {
@@ -13,14 +14,44 @@ type Handler struct {
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := filepath.Join(h.Static, r.URL.Path)
+	fileType := filepath.Ext(path)
+
+	acceptGzip := strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
 
 	// Check whether a file exists or is a directory
 	if fi, err := os.Stat(path); os.IsNotExist(err) || fi.IsDir() {
-		http.ServeFile(w, r, filepath.Join(h.Static, h.Index))
+		htmlFile := filepath.Join(h.Static, h.Index)
+		if acceptGzip {
+			if _, err := os.Stat(htmlFile + ".gz"); err == nil {
+				w.Header().Add("Content-Encoding", "gzip")
+				w.Header().Add("Content-Type", "text/html")
+				htmlFile += ".gz"
+			}
+		}
+
+		// Serve SPA
+		http.ServeFile(w, r, htmlFile)
 		return
 	} else if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+
+	// Serve gziped
+	if acceptGzip && (fileType == ".js" || fileType == ".css") {
+		if _, err := os.Stat(path + ".gz"); err == nil {
+
+			w.Header().Add("Content-Encoding", "gzip")
+			if fileType == ".js" {
+				w.Header().Add("Content-Type", "text/javascript")
+			}
+			if fileType == ".css" {
+				w.Header().Add("Content-Type", "text/css")
+			}
+
+			http.ServeFile(w, r, filepath.Join(path+".gz"))
+			return
+		}
 	}
 
 	w.Header().Add("Cache-Control", "no-cache")
